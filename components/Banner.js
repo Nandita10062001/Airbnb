6db69f805e3d2c93fd79f863749705e6dccaa53b
Nandita <nanditankr106@gmail.com>
2021-11-27 02:07:13 +0530

    Banner part Completed

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 function Banner() {
     return (
-        <div>
-            
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:[700px]">
+            <Image src="https://links.papareact.com/0fm" layout="fill" objectFit="cover"/>
+
+            <div className="absolute top-1/2 w-full text-center">
+             <p className="text-sm sm:text-lg"> Not sure where to go? Perfect.</p>
+             <button className="text-purple-500 bg-white px-10 py-4 rounded-full shadow-md font-bold my-3 hover:shadow-xl active:scale-90 trasition duration-150">I'm Flexible</button>
+            </div>
         </div>
     )
 }
