99a3de24e94d87e84ce8203b0c86eaaa3cc33e14
Nandita <nanditanandakumar19@gmail.com>
2021-11-27 19:15:36 +0530

    frontend

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+             <h5 className="font-bold">ABOUT</h5>
+             <p>How Airbnb Works</p>
+             <p>Newsroom</p>
+             <p>Investors</p>
+             <p>Airbnb Plus</p>
+             <p>Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+             <h5 className="font-bold">HOST</h5>
+             <p>
+             Nandita Nandakumar</p>
+             <p>
+             Third Year Computer Science</p>
+             <p>Web Development</p>
+             <p>Next JS</p>
+             <p>Tailwind CSS</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+             <h5 className="font-bold">COMMUNITY</h5>
+             <p>Accessibility</p>
+             <p>This is not the real site</p>
+             <p>It is an Amazing CLone</p>
+             <p>Referrals Accepted</p>
+             <p>Nanditankr</p>
+            </div>
+        
+            <div className="space-y-4 text-xs text-gray-800">
+             <h5 className="font-bold">SUPPORT</h5>
+             <p>Help Centre</p>
+             <p>Trust & Safety</p>
+             <p>Say Hi YouTube</p>
+             <p>Happy ThanksGiving</p>
+             <p>Let's Go</p>
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default Footer
+
