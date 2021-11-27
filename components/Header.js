0097c55e9543d710f1b6bedeb0978c44d9b55004
Nandita <nanditanandakumar19@gmail.com>
2021-11-27 19:46:17 +0530

    fixed globe icon

--- components/Header.js
+++ components/Header.js
@@ -33,7 +33,7 @@ function Header() {
       {/*Right*/}
       <div className="flex items-center space-x-4 justify-end text-gray-500">
         <p className="hidden md:inline pl-2 cursor-pointer">Become a Host</p>
-        <GlobeAltIcon className="h-6" />
+        <GlobeAltIcon className="h-8 md:h-6 " />
         <div className="flex item curs-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
           <MenuIcon className="h-6" />
           <UserCircleIcon className="h-6" />
