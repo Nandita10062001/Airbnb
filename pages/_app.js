757361c769ede4a9e7c42456ccd88aee0942f193
Nandita <nanditanandakumar19@gmail.com>
2021-11-30 02:56:02 +0530

    Added Loading Functionality

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import ProgressBar from "@badrap/bar-of-progress";
 import Router from "next/router";
 
 const progress = new ProgressBar({
-  size: 2,
+  size: 4,
   color: "#FE595E",
   className: "z-50",
   delay: 100,
