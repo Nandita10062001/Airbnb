dac973991a82d6b52638be67b423071e1ae636fd
Nandita <nanditanandakumar19@gmail.com>
2021-11-29 21:25:07 +0530

    Implemented Search Functionality

--- components/Header.js
+++ components/Header.js
@@ -10,17 +10,18 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css"; //main style file
 import "react-date-range/dist/theme/default.css"; //theme style file
 import { DateRangePicker } from "react-date-range";
+
 function Header() {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
 
-  
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
-  }
-
+  };
+  const resetInput = () => {setSearchInput('')};
   const selectionRange = {
     startDate: startDate,
     endDate: endDate,
@@ -68,6 +69,24 @@ function Header() {
             rangeColors={["#FD5B61"]}
             onChange={handleSelect}
           />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold pl-2">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />{" "}
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
         </div>
       )}
     </header>
